649cd2a2877673694fa93ca0dea26b9374754396
macsen110 <liangyusen1202@gmail.com>
2015-01-05 20:55:36 +0800

    lys global

--- script/LYS.js
+++ script/LYS.js
@@ -1,4 +1,9 @@
+/**********
+ * Vision 1.1.
+ * ***********/
 var LYS = LYS || function(q,c){return LYS.dom?LYS.dom(q,c):null};
+
+//设置global
 LYS.global = LYS.global || (function() {
     var global = global|| {};
     return function( key, value, overwrite ) {
@@ -9,7 +14,12 @@ LYS.global = LYS.global || (function() {
         } else if (key && typeof global[ key ] == "undefined" ) {
             global[ key ] = {};
         }
-
         return global[ key ];
     }
 })();
+
+//创建扩展方法，数据类型判断
+
+LYS.type = function(){
+
+}
