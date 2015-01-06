3d62b1fa24579664a2f8ebcb00ddf8d95440b900
macsen110 <liangyusen1202@gmail.com>
2015-01-06 21:21:10 +0800

    auto

--- script/LYS.js
+++ script/LYS.js
@@ -19,7 +19,27 @@ LYS.global = LYS.global || (function() {
 })();
 
 //创建扩展方法，数据类型判断
-
-LYS.type = function(){
-
+LYS.type = (function(){
+    var str = "Object Boolean Date Error Function Number RegExp String";
+    Array.forEach(str.split(" "), function(name) {
+        LYS[ "is" + name ] = function ( unknow ) {
+            return LYS.type(unknow) == name.toLowerCase();
+        }
+    });
+    // 方法主体
+    return function ( unknow ) {
+        var type = typeof unknow;
+        if(type=='object'){
+            if(Object.prototype.toString.call(unknow) == '[object Array]'){
+                type = 'array'
+            }
+        }
+        return type;
+    };
+}());
+//数组与一般对象的判断稍微不一样
+LYS.isArray =  function(unknow){
+    return Object.prototype.toString.call(unknow) == '[object Array]';
 }
+
+
