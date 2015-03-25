898e8c0191c7be8febb8eb3ac36ec6d4421b091f
macsen110 <liangyusen@gmail.com>
2015-03-26 00:08:36 +0800

    add functionality module

--- http.js
+++ http.js
@@ -2,66 +2,30 @@
 var //restify = require('restify'),
     //ecstatic = require('ecstatic'),
     url  = require("url"),
-    path = require("path"),
+    //path = require("path"),
     http = require('http'),
-    fs   = require("fs"),
+    //fs   = require("fs"),
+    readUri = require('./readUri');
     server = new http.Server();
 
-
+server.name = "www.showtime.com";
 server.on('request',function(req,res,next){
-
-    //res.end('oopssss');
-
     var pathname=__dirname+url.parse(req.url).pathname;
     var api = req.url;
-    console.log(api);
-    if (path.extname(pathname)=="") {
-        pathname+="/";
-    }
-    if (pathname.charAt(pathname.length-1)=="/"){
-        pathname=__dirname+"/index.html";
-    }
-    fs.exists(pathname,function(exists){
-        if(exists){
-            switch(path.extname(pathname)){
-                case ".html":
-                    res.writeHead(200, {"Content-Type": "text/html"});
-                    break;
-                case ".js":
-                    res.writeHead(200, {"Content-Type": "text/javascript"});
-                    break;
-                case ".css":
-                    res.writeHead(200, {"Content-Type": "text/css"});
-                    break;
-                case ".gif":
-                    res.writeHead(200, {"Content-Type": "image/gif"});
-                    break;
-                case ".jpg":
-                    res.writeHead(200, {"Content-Type": "image/jpeg"});
-                    break;
-                case ".png":
-                    res.writeHead(200, {"Content-Type": "image/png"});
-                    break;
-                default:
-                    res.writeHead(200, {"Content-Type": "application/octet-stream"});
-            }
+    if (api.indexOf('api') === 1) {
 
-            fs.readFile(pathname,function (err,data){
-                res.end(data);
-            });
-        } else {
-            res.writeHead(404, {"Content-Type": "text/html"});
-            res.end("<h5>404 Not Found</h5>");
+    }
+    else{
+        if (api!=="/" && api.indexOf('.html') === -1) {
+            pathname+=".html";
         }
-    })
-})
-//get也可以添加正则
-//server.get('/',restify.serveStatic({
-//   directory: './',
-//    default: 'index.html'
-//}))
-//server.head('/hello/:name', respond);
+        else {
+            pathname += "index.html"
+        }
+    }
+    readUri.read(pathname,res);
 
-server.listen(3000,'www.showtime.com', function() {
-    console.log('%s listening at %s', server.name, server.url);
+})
+server.listen(3000,server.name, function() {
+    console.log('%s are listening', server.name);
 });
---
+++ readUri.js
@@ -0,0 +1,40 @@
+exports.read = function(pathname,res) {
+	var fs = require('fs');
+	var path = require('path');
+	    fs.exists(pathname,function(exists){
+        if(exists){
+            switch(path.extname(pathname)){
+                case ".html":
+                    res.writeHead(200, {"Content-Type": "text/html"});
+                    break;
+                case ".js":
+                    res.writeHead(200, {"Content-Type": "text/javascript"});
+                    break;
+                case ".css":
+                    res.writeHead(200, {"Content-Type": "text/css"});
+                    break;
+                case ".gif":
+                    res.writeHead(200, {"Content-Type": "image/gif"});
+                    break;
+                case ".jpg":
+                    res.writeHead(200, {"Content-Type": "image/jpeg"});
+                    break;
+                case ".png":
+                    res.writeHead(200, {"Content-Type": "image/png"});
+                    break;
+                case ".svg" :
+                    res.writeHead(200, {"Content-Type" : "image/svg+xml"});
+                    break;
+                default:
+                    res.writeHead(200, {"Content-Type": "application/octet-stream"});
+            }
+
+            fs.readFile(pathname,function (err,data){
+                res.end(data);
+            });
+        } else {
+            res.writeHead(404, {"Content-Type": "text/html"});
+            res.end("<h5>404 Not Found</h5>");
+        }
+    })
+}
