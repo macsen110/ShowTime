3138e57593a10968d2d15b3971036715c3368a58
macsen110 <liangyusen1202@gmail.com>
2015-06-01 18:20:17 +0800

    'es6'

--- script/es6/es6-demo.js
+++ script/es6/es6-demo.js
@@ -88,40 +88,34 @@ class Greeter {
 	}
 }
 
-var greeter = new Greeter('Hello, world!');
+var greeter = new Greeter('class test Hello, world!');
 greeter.greet();
 
 class GreeterChild extends Greeter {
 	greet() {
-		var element = document.querySelector('#message');
+		var element = document.querySelector('#extends-message');
 		element.innerHTML = this.message + 1;
 	}
 }
 
-var greeterChild = new GreeterChild('oops');
+var greeterChild = new GreeterChild('extend class test oops');
 greeterChild.greet();
-//set 是一种数据结构,类似于数组,类型为对象,不是数组
+
+
+console.log('%cSet 是一种数据结构,类似于数组,类型为对象,不是数组,具有size,add,delete,has,clear,values方法',"color:blue;font-size:x-large");
 let testSet = new Set();
 testSet.add(1)
-console.log(testSet.has(1));
-
+console.log('Set 数据方法返回布尔值',testSet.has(1));
 let testSetArr = new Set([1,1,2,2,4]);
-
-
-
 for (let i of testSetArr) {
-    console.log('testSetArr'+ i)
+    console.log('for of 循环 set 数据格式'+ i)
 }
-
-testSetArr.forEach((value, key) => value*2 );
-
-console.log(testSetArr);
 //很方便数组去重,貌似某些brower 不支持
 var uniqueArr = Array.from(new Set([1,4]));
-console.log(uniqueArr);
+console.log('数组去重Array.from(Set)',uniqueArr);
 //另外的一种去重
 let uniqueArr2 = [...testSetArr];
-console.log(uniqueArr2)
+console.log('数组扩展运算符去重"[...Set]"',uniqueArr2)
 
 //实现交集,并集
 
@@ -132,7 +126,38 @@ console.log('并集实现' + [...union]);
 let intersect = new Set([...a].filter(x => b.has(x)));
 console.log('交集实现' + [...intersect]);
 
-//Map
+
+
+console.log('%cMap 数据结构,类似与对象,键值对的集合,键的范围不限于字符串,方法size,set,get,delete,has',"color:blue;font-size:x-large");
+let map = new Map([["aa","namea"],["bb","namebb"]]);
+console.log("map size 属性 代表长度",map.size)
+
+console.log('%cES6 函数扩展','color:blue;font-size:x-large')
+//函数的默认参数
+function defaulParamstFun(params=0) {
+	console.log('函数的默认参数,必须是函数的尾部参数',params)
+}
+defaulParamstFun();
+
+//函数的rest参数,(...变量名);rest 参数搭配的变量名是一个数组,该变量把多余的参数放入其中,rest参数后不能有其他参数
+function restFun(...params) {
+	var sum = 0;
+	for (let i of params) {
+		sum += i
+	} 
+	return sum
+}
+
+console.log('函数的rest参数,(...变量名);rest 参数搭配的变量名是一个数组,该变量把多余的参数放入其中',restFun(1,2,3))
+
+//扩展运算符...好比rest参数的逆运算,把一个数组转换成逗号分隔的参数序列
+console.log('Math.max(...[1,2,3])',Math.max(...[1,2,3]))
+var spreatString = [..."hello"];
+console.log('扩展运算符还可以将字符串扩展成数组', spreatString)
+console.log('扩展运算符内部调用的是数据结构的Iterator接口,因此只要具有Iterator接口的对象,都可以使用扩展运算符')
+
+
+
 
 //Generator 函数
 function* f() {
@@ -152,19 +177,23 @@ console.log(g.next(true));
 //es6 js模板
 var tempVarible = 'i \'m ok';
 var c = `ssss
-<b>aa${tempVarible}aa</b>`;
-
+<div>aa${tempVarible}aa</div>`;
+document.body.insertAdjacentHTML('beforeend',c);
 console.log(String.raw`aaa`);
 console.log(Array.of(1,2,3,4));
 let [DestructA,DestructB] = [0,1];
 console.log(DestructA);
 var {foo, bar} = {foo:'xxxx', bar:'yyyy'};
 
-//函数的默认参数
-function name(params = 0) {
-	alert(params)
+
+
+if (2>1) {
+	let bLet = [3,2,1];
+	bLet.forEach(x => {console.log('箭头函数',x);5})
 }
-name();
+
+
+
 
 //import module in es6
 
