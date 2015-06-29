498a4e3ebb19467819cd84abd2dc6e408c503050
macsen110 <liangyusen1202@gmail.com>
2015-06-29 18:05:13 +0800

    requestAnimationFrame

--- script/css3/animate.js
+++ script/css3/animate.js
@@ -1,15 +1,27 @@
-window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+// window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 
-var start = null;
-var d = document.getElementById('SomeElementYouWantToAnimate');
-function step(timestamp) { 
-  if (start === null) start = timestamp;
-  var progress = timestamp - start;
-  console.log(progress)
-  d.style.left = Math.min(progress/10, 200) + "px";
-  if (progress < 2000) {
-    console.log('timestamp :' + timestamp + ' start: ' + start + ' progress: ' + progress )
-    requestAnimationFrame(step);
+// var start = null;
+// var d = document.getElementById('SomeElementYouWantToAnimate');
+// function step(timestamp) { 
+//   if (start === null) start = timestamp;
+//   var progress = timestamp - start;
+//   console.log(progress)
+//   d.style.left = Math.min(progress/10, 200) + "px";
+//   if (progress < 2000) {
+//     console.log('timestamp :' + timestamp + ' start: ' + start + ' progress: ' + progress )
+//     requestAnimationFrame(step);
+//   }
+// }
+// requestAnimationFrame(step);
+
+
+function updateProgress(timestamp) {
+  var div = document.getElementById('status');
+  div.style.width = (parseInt(div.style.width, 10) + 1) + '%';
+  console.log(div.style.width);
+  if (div.style.width != "100%") {
+    requestAnimationFrame(updateProgress);
   }
 }
-requestAnimationFrame(step);
+
+requestAnimationFrame(updateProgress)
