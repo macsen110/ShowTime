a70c6887332fbd604c696a5bdf7d6d59191028ad
macsen110 <liangyusen1202@gmail.com>
2015-08-14 20:10:01 +0800

    'reasearch'

--- app.js
+++ app.js
@@ -7,6 +7,78 @@ server.name = "www.showtime.com";
 server.on('request',function(req,res,next){
     var pathname=__dirname+url.parse(req.url).pathname;
     var api = req.url;
+    if (api === '/api/research/start') {
+        res.end(JSON.stringify({
+            code: 0,
+            componentName: 'Start',
+            dataObj: {
+                desc: '开始'
+            }
+        }),'utf-8');
+
+    }
+    if (api === '/api/research/info') {
+        res.end(JSON.stringify({
+            code: 0,
+            componentName: 'Information',
+            dataObj: {
+                dataArr:[
+                    {type:'ipt',name:'姓名',keys:'name'},
+                    {type:'ipt',name:'手机号码',keys:'phone'}
+                ]
+            },
+        }),'utf-8');
+    }
+    
+    if (api === '/api/research/question1') {
+        res.end(JSON.stringify({
+            code: 0,
+            componentName: 'Question_radio',
+            dataObj: {
+                title: 'Question_radio component',
+                data: [1,2,3]
+            }
+        }),'utf-8');
+    }
+    
+    if (api === '/api/research/question2') {
+        res.end(JSON.stringify({
+            code: 0,
+            componentName: 'Question_input',
+            dataObj: {
+                title: 'Question_input component',
+                data: [1,2,3]
+            }
+        }),'utf-8');
+    }
+    
+    if (api === '/api/research/question_checkbox') {
+        res.end(JSON.stringify({
+            code: 0,
+            componentName: 'Question_checkbox',
+            dataObj: {
+                title: 'Question_checkbox component',
+                data: [1,2,3]
+            }
+        }),'utf-8');
+    }
+    
+    if (api === '/api/research/suggest') {
+        res.end(JSON.stringify({
+            code: 0,
+            componentName: 'Suggest',
+        }),'utf-8');
+    }
+    
+    if (api === '/api/research/end') {
+        res.end(JSON.stringify({
+            code: 0,
+            componentName: 'End',
+            dataObj: {
+                desc: 'End',
+            }
+        }),'utf-8');
+    }
     if (api.indexOf('/api/') !== -1) {
         var apiHref = api.slice(api.indexOf('/api/')+5);
         console.log(apiHref);
--- script/react/research/dev.js
+++ script/react/research/dev.js
@@ -12,220 +12,273 @@ function nextPage(hash) {
     window.location.hash = hash;
 }
 
-class C0 extends React.Component {
+
+//开始答题,说明
+class C_start extends React.Component {
     render() {
         return (
-            <div className = "comm-box-0" >
-                <p className="desc">
-                    感谢您参与此次调查，本调查目的在于
-                    了解产品对消费者购物选择的影响。您
-                    对本问卷的所有回答都是以匿名形式进
-                    行并且答案得到严格保密，研究结果将
-                    是结论性质的报告，不会泄漏您的任何
-                    相关信息。每位参与调研的朋友都将获
-                    得我们送出的尊贵礼品，同时你还将参
-                    加抽奖，有机会获得iphone6等大奖。
-                </p>
-                <p className="flex"><button onClick= {nextPage.bind(null,'page1')} className="btn">开始答题</button></p>              
+            <div className = "comm-box-start" >
+                <p className="desc">{this.props.desc}</p>
+                <p className="flex"><button onClick= {nextPage.bind(null,'/api/research/info')} className="btn">开始答题</button></p>              
             </div>
                         
             )
     }
 }
-class C1 extends React.Component {
+
+//用户信息,
+class C_info extends React.Component {
     handleSubmit() {
-        nextPage('page2')
+        nextPage('/api/research/question1')
     }
     render() {
+        var props = this.props;
         return (
-            <div className="comm-box-1">
+            <div className="comm-box-information">
                 <p className="title t-c">请先填写您的资料</p>
-                <p>请输入您的姓名：</p>
-                <input type="text" className="ipt" />
-                <p>请输入您的手机号码:</p>
-                <input type="text" className="ipt" />
-                <p className="tip">*建议填写正确的手机号</p>
+                <C_info_container dataArr={props.dataArr} />
                 <div className = "flex">
                     <button className="btn sub" onClick={back}>返回</button>
                     <button className="btn" onClick={this.handleSubmit}>马上开始</button>
                 </div>
             </div>
-            )
+        )
     }
 }
 
 
-class C2 extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            title: '',
-            colors: []
-        }
-        
-    }
-
-    componentWillMount() {
-        var self = this;
-        new xhr({
-            url:'data/colorChoice.js',
-            done: function(obj) {
-                if (obj.code == 0) {
-                    self.setState({
-                       title: obj.title,
-                       colors: obj.data
-                    }) 
-                }
-
-            }
-        })
-    }
+//用户信息输入判断
+class C_info_container extends React.Component {
     render() {
-        var state = this.state;
-        if (state.colors.length) {
+        return (
+            <ul className="list">        
+                {this.props.dataArr.map((item, index) => {
+                    switch (item.type) {
+                        case 'ipt':
+                            return <C_info_ipt {...item} key={index} /> 
+                    }
+                })}
+                
+            </ul>
+        )
+    }
+}
+
+//用户需要输入信息的类型
+
+//输入型
+class C_info_ipt extends React.Component {
+    render() {
+        var props = this.props;
+        return (
+            <li className="item">
+                <p>请填写您的{props.name}：</p>
+                <input className="ipt" name={props.key} />
+                {props.keys == 'phone' ? <p className="tip">*建议填写正确的手机号</p> : ''}
+            </li>
+        )
+    } 
+}
+//选择型---单选
+class C_info_radio extends React.Component {
+    
+}
+
+
+//问题类型,单选题
+class C_question_radio extends React.Component {
+    render() {
             return (
-                <div className="comm-box-2">
-                    <p className="title">{state.title}</p>
+                <div className="comm-box-radio">
+                    <p className="title">{this.props.title}</p>
                     <p>*注：本题最多能选1个答案</p>
                     <ul>
-                        {state.colors.map((val, index) => <li key={index}><input type="radio" name="checkColor" value={val} />{val}</li>)}
+                        {this.props.data.map((val, index) => <li className="item" key={index}><input type="radio" name="checkColor" value={val} />{val}</li>)}
                     </ul>
-                    <div className = "flex">
-                        <button className="btn sub" onClick={back}>返回</button>
-                        <button className="btn" onClick={this.handleSubmit}>马上开始</button>
+                    <div className ="flex">
+                        <button className="btn sub" onClick={back}>上一题</button>
+                        <button className="btn" onClick={this.handleSubmit}>下一题</button>
                     </div>
                 </div>
             )
-        }
-        return (<div></div>)
+        
 
     }
     handleSubmit() {
-        nextPage('page3')
+        nextPage('/api/research/question2')
     }
 }
 
-
-class C3 extends React.Component {
+//问题类型-复选题
+class C_question_checkbox extends React.Component {
     handleSubmit() {
-        nextPage('page4')
+        nextPage('/api/research/suggest')
     }
     render() {
         return (
-            <div className="comm-box-3">
-                <p className="title">()是你最喜欢的足球运动员</p>
-                <p>*注：本题最多能选1个答案</p>
+            <div className="comm-box-check">
+                <p className="title">{this.props.title}</p>
+                <p>*注：本题能选多个答案</p>
+                    <ul>
+                        {this.props.data.map((val, index) => <li className="item" key={index}><input type="checkbox" name={this.props.name} value={val} />{val}</li>)}
+                    </ul>
+                <div className = "flex">
+                    <button className="btn sub" onClick={back}>上一题</button>
+                    <button className="btn" onClick={this.handleSubmit}>下一题</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+//问题类型,填写内容
+class C_question_ipt extends React.Component {
+    handleSubmit() {
+        nextPage('/api/research/question_checkbox')
+    }
+    render() {
+        return (
+            <div className="comm-box-input">
+                <p className="title">{this.props.title}</p>
+                <p>请输入:</p>
+                <div className="wrap-ipt">
+                    <textarea></textarea>
+                </div>
+                <div className = "flex">
+                    <button className="btn sub" onClick={back}>上一题</button>
+                    <button className="btn" onClick={this.handleSubmit}>下一题</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+//用户建议,
+class C_user_suggest extends React.Component {
+    handleSubmit() {
+        nextPage('/api/research/end')
+    }
+    render() {
+        return (
+            <div className="comm-box-suggest">
+                <p className="t-c">感谢您的参与</p>
+                <div className="wrap-ipt">
+                    <textarea placeholder="有什么建议留给我们？"></textarea>
+                </div>
                 <div className = "flex">
                     <button className="btn sub" onClick={back}>返回</button>
-                    <button className="btn" onClick={this.handleSubmit}>马上开始</button>
+                    <button className="btn" onClick={this.handleSubmit}>完成</button>
                 </div>
             </div>
             )
     }
 }
 
-
-class C4 extends React.Component {
-    handleSubmit() {
-        nextPage('page5')
-    }
+//结束语
+class C_end extends React.Component {
     render() {
         return (
-            <div className="comm-box-4">
-                <p className="title">()是你最喜欢的足球运动员</p>
-                <p>*注：本题最多能选1个答案</p>
-                <div className = "flex">
-                    <button className="btn sub" onClick={back}>返回</button>
-                    <button className="btn" onClick={this.handleSubmit}>马上开始</button>
-                </div>
-            </div>
-            )
+            <div className="comm-box-end">{this.props.desc}</div>
+        )
     }
 }
 
-
-class C5 extends React.Component {
-    handleSubmit() {
-        nextPage('page6')
-    }
+class Loading extends React.Component {
     render() {
-        return (
-            <div className="comm-box-5">
-                <p>你喜欢什么颜色？</p>
-                <p>*注：本题最多能选1个答案</p>
-                <div className = "flex">
-                    <button className="btn sub" onClick={back}>返回</button>
-                    <button className="btn" onClick={this.handleSubmit}>马上开始</button>
-                </div>
-            </div>
-            )
-    }
-}
-
-
-class C6 extends React.Component {
-    render() {
-        return (
-            <div className="comm-box-5">
-                <p>尊敬的客户：</p>
-                <p>
-                    感谢您的耐心参与，您的选择，将对我们起到重要的指导作用，我们在此向您表示诚挚的谢意。
-                    同时，为了回馈您的贡献，我们将从所有参与调研的客户中随机抽取几位幸运奖得主。您将有机会
-                    获得我们为您精心准备的礼品一份，中奖名单将在近期公布，敬请关注。祝您生活愉快。
-                </p>
-               
-            </div>
-            )
+        return (<div>Loading</div>)
     }
 }
 
 class All extends React.Component {
     constructor(props) {
         super(props)
-        var curLinkIndex = location.hash ? Number(location.hash[location.hash.length - 1]) : 0
+        var curLink = location.hash ? location.hash.slice(1) : '/api/research/start';
         this.state =  {
-            curLink: curLinkIndex
+            ready: false,
+            curLink: curLink
         };
     }
+    loadAnsyData(url) {
+       this.setState({
+           ready: false
+       })
+       var self = this;
+        new xhr({
+            url:url,
+            done: function (obj) {
+                if (obj.code == 0) {
+                    self.setState({
+                        ready: true,
+                        componentName: obj.componentName,
+                        dataObj: obj.dataObj
+                    })
+
+                }                              
+            }
+        })
+        
+    }
     componentDidMount() {
-        window.addEventListener('hashchange', function () {
-            this.setState({curLink: Number(location.hash[location.hash.length - 1])})
+        var self =this;
+        new xhr({
+            url: self.state.curLink,
+            done: function (obj) {
+                if (obj.code == 0) {
+                    self.setState({
+                        componentName: obj.componentName,
+                        ready: true,
+                        dataObj: obj.dataObj
+                    })
+                }                              
+            }
+        })
+        window.addEventListener('hashchange', function () {          
+            var url = location.hash ? location.hash.slice(1) : '/api/research/start'
+            this.loadAnsyData(url)
         }.bind(this))
     }
     render() {
-        var curIndex = this.state.curLink;
-        var component;
-        switch(curIndex) {
-            case 1:
-                component = <C1 />
-                break;
-            case 2:
-                component = <C2 />
-                break;
-            case 3:
-                component = <C3 />
-                break;
-            case 4:
-                component = <C4 />
-                break;
-            case 5:
-                component = <C5 />
-                break;
-            case 6:
-                component = <C6 />
-                break;
-            case 0 :
-            default: 
-                component = <C0 />
-                break;
+        if (this.state.ready) {
+            var componentName = this.state.componentName;
+            var dataObj = this.state.dataObj;
+            var component;
+            switch(componentName) {
+                case 'Start':
+                    component = <C_start {...dataObj} />
+                    break;
+                case 'Information':
+                    component = <C_info {...dataObj} />
+                    break;
+                case 'Question_radio':
+                    component = <C_question_radio {...dataObj} />
+                    break;
+                case 'Question_checkbox':
+                    component = <C_question_checkbox {...dataObj} />
+                    break;
+                case 'Question_input':
+                    component = <C_question_ipt {...dataObj} />
+                    break;
+                case 'Suggest':
+                    component = <C_user_suggest />
+                    break;
+                case 'End':
+                    component = <C_end {...dataObj} />
+                    break;
+                default: 
+                    break;
+            }
+
+            return (
+                <div className="comm-box">               
+                    {component}
+                </div>
+            )
         }
-        return (
-            <div className="comm-box">
-                {component}
-            </div>
-        )
+        return (<div><Loading /></div>)
+
     }
 }
+
 
 
 
